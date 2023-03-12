a004f91491ed497b7aeebf781d9ef59d98f29ab9
s.sachenko <braun.krieg@gmail.com>
2023-03-12 16:39:14 +0300

    alert

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-jquery(document).ready(function () {});
+alert("Добро пожаловать!");
+confirm("Вам нравится наш сайт?")
+  ? alert("Отлично, спасибо!")
+  : alert("Очень грустно!");
+
+$(document).ready(function () {});
